8123457dbbc1d5db5d243136b80597c26ef44d14
aob-prog <andres.benitez@gba.gob.ar>
2021-04-10 21:35:00 -0300

    cuarta subida

---
+++ arrays.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Primera pagina Php</title>
+   
+</head>
+<body>
+
+<?php
+   
+$datos=array("Nombre"=>"Juan","Apellido"=>"Gomez","Edad"=>21);
+
+echo $datos["Apellido"];
+
+
+
+?>
+
+</body>
+
+</html>
